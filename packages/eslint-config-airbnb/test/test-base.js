500858f6b1f4828c7bd070e34b8eced620d5a608
shodan_uk <terry.morgan76@gmail.com>
2016-02-24 16:12:45 +0000

    Indenting

--- packages/eslint-config-airbnb/test/test-base.js
+++ packages/eslint-config-airbnb/test/test-base.js
@@ -3,28 +3,28 @@ import path from 'path';
 import test from 'tape';
 
 const files = {
-  base: require('../base')
+    base: require('../base')
 };
 
 fs.readdirSync(path.join(__dirname, '../rules')).forEach(name => {
-  if (name === 'react.js') {
-    return;
-  }
+    if (name === 'react.js') {
+        return;
+    }
 
-  files[name] = require(`../rules/${name}`);
+    files[name] = require(`../rules/${name}`);
 });
 
 Object.keys(files).forEach(name => {
-  const config = files[name];
+    const config = files[name];
 
-  test(`${name}: does not reference react`, t => {
-    t.plan(2);
+    test(`${name}: does not reference react`, t => {
+        t.plan(2);
 
-    t.notOk(config.plugins, 'plugins is unspecified');
+        t.notOk(config.plugins, 'plugins is unspecified');
 
-    // scan rules for react/ and fail if any exist
-    const reactRuleIds = Object.keys(config.rules)
-      .filter(ruleId => ruleId.indexOf('react/') === 0);
-    t.deepEquals(reactRuleIds, [], 'there are no react/ rules');
-  });
+        // scan rules for react/ and fail if any exist
+        const reactRuleIds = Object.keys(config.rules)
+            .filter(ruleId => ruleId.indexOf('react/') === 0);
+        t.deepEquals(reactRuleIds, [], 'there are no react/ rules');
+    });
 });
